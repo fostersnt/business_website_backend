c1765ff3233fe1d14e0e9fc564fa1e11258944d7
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-07 12:15:07 +0000

    Updated the api response

--- database/usersQuery.js
+++ database/usersQuery.js
@@ -1,6 +1,7 @@
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
 
+//INSERT USER DATA
 exports.insertUser = async (userData) => {
   try {
     const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
@@ -22,3 +23,26 @@ exports.insertUser = async (userData) => {
     };
   }
 };
+
+
+//UPDATE USER DATA
+exports.updateUser = async (userData) => {
+    try {
+      const query = "UPDATE users SET name = ?, email = ?";
+      const updateData = [userData.name, userData.email];
+      const result = await db.query(query, updateData);
+      // return result[0];
+      return {
+          isError: false,
+          data: result[0],
+          model_name: "User"
+      };
+    } catch (err) {
+      //LOG ERROR MESSAGE HERE
+      return {
+          isError: true,
+          data: null,
+          model_name: "User"
+      };
+    }
+  };
--- responses/apiResponse.js
+++ responses/apiResponse.js
@@ -1,7 +1,35 @@
+//INSERT RESPONSE
 exports.insertResponse = (isError, itemId, model_name) => {
   return {
     isError: isError,
     [`${model_name}Id`]: itemId,
     message: `${model_name} has been created successfully`,
+  };
+};
+
+//UPDATE RESPONSE
+exports.updateResponse = (isError, itemId, model_name) => {
+  return {
+    isError: isError,
+    [`${model_name}Id`]: itemId,
+    message: `${model_name} has been updated successfully`,
+  };
+};
+
+//GET RESPONSE
+exports.getResponse = (isError, data) => {
+    return {
+      isError: isError,
+      data: data,
+      message: "Success",
+    };
+  };
+
+//DELETE RESPONSE
+exports.deleteResponse = (isError, itemId, model_name) => {
+  return {
+    isError: isError,
+    [`${model_name}Id`]: itemId,
+    message: `${model_name} has been deleted successfully`,
   };
 };
